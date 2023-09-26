4987e3fe8b83391a9c1a2307dfc893b7fee6d3ba
Shane Walsh <shanewalsh@hey.com>
2023-09-27 00:41:07 +0100

    adds socket listeners

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,164 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+
+import './App.css';
+
+import dayjs from 'dayjs';
+
+import io from 'socket.io-client';
+import { type Message } from './data-access';
+
+const socket = io('http://192.168.1.155:3000');
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState<string>();
+  const [messages, setMessages] = useState<Array<Message>>([]);
+  const [typer, setTyper] = useState<string>();
+  const [message, setMessage] = useState<string>('');
+
+  useEffect(() => {
+    socket.on('messages_updated', (messages: Array<Message>) => {
+      setMessages(messages);
+    });
+
+    socket.on('send_typing', (data) => setTyper(data?.username));
+  }, []);
+
+  const loggedInName = username || localStorage.getItem('username');
+
+  if (!loggedInName) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <form
+          style={{
+            gap: '1rem',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+          onSubmit={(e) => {
+            e.preventDefault();
+            const username = e.currentTarget.username.value;
+            localStorage.setItem('username', username);
+            setUsername(username);
+            socket.emit('join_room', { username });
+          }}
+        >
+          <input
+            required
+            name="username"
+            placeholder="What's your name?"
+            style={{
+              padding: '1rem',
+              fontSize: '1.5rem',
+            }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>Hi, {loggedInName}!</p>
+        <button
+          onClick={() => {
+            localStorage.removeItem('username');
+            setTimeout(() => {
+              setUsername(undefined);
+            });
+          }}
+        >
+          Log out
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Real Time Messages!</h1>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+        }}
+      >
+        {messages.map((message) => {
+          const isAuthor = message.author === loggedInName;
+
+          return (
+            <div
+              key={message.id}
+              className="card"
+              style={{
+                background: isAuthor ? 'rgb(62, 123, 247)' : 'slategray',
+                borderRadius: '1rem',
+                boxShadow: '0 0 1rem rgba(0,0,0,0.5)',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: isAuthor ? 'flex-end' : 'flex-start',
+              }}
+            >
+              <div style={{ display: 'flex', gap: '8px', alignItems: 'baseline' }}>
+                <p
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: '1.5rem',
+                  }}
+                >
+                  {message.author}
+                </p>
+                <p>at {dayjs(message.createdAt).format('DD-MM-YY - hh:mm')}</p>
+              </div>
+              <p>{message.content}</p>
+            </div>
+          );
+        })}
+        {typer && <p>{typer} is typing...</p>}
+
+        <form
+          style={{
+            gap: '1rem',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+          onSubmit={(e) => {
+            e.preventDefault();
+            const content = e.currentTarget.content.value;
+            const author = loggedInName;
+
+            // createMessage({ content, author });
+
+            socket.emit('send_message', { content, author });
+            socket.emit('stop_typing');
+            setMessage('');
+          }}
+        >
+          <input
+            required
+            value={message}
+            onChange={(e) => {
+              setMessage(e.currentTarget.value);
+              if (e.currentTarget.value.length > 0) {
+                socket.emit('typing', { username: loggedInName });
+              } else {
+                socket.emit('stop_typing');
+              }
+            }}
+            name="content"
+            placeholder="What's your message?"
+            style={{
+              padding: '8px',
+              fontSize: '1.5rem',
+            }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/data-access/messages.ts
@@ -0,0 +1,28 @@
+const url = 'http://192.168.1.155:3000';
+const path = '/messages';
+
+const messagesUrl = `${url}${path}`;
+
+export type Message = {
+  id: string;
+  content: string;
+  author: string;
+  createdAt: string;
+};
+
+export type NewMessageReq = Pick<Message, 'content' | 'author'>;
+
+export async function getMessages(): Promise<Array<Message>> {
+  return await (await fetch(messagesUrl)).json();
+}
+
+export async function createMessage(message: NewMessageReq): Promise<Message> {
+  const response = await fetch(messagesUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+  return await response.json();
+}
